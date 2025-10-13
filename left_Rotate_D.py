db2b809750bf0a568f8d1476b80972af4ac468f1
AnanyaNaik <naikananya02@gmail.com>
2025-10-13 17:02:45 +0530

    left_Rotate_D code is modified

--- left_Rotate_D.py
+++ left_Rotate_D.py
@@ -1,19 +1,22 @@
-def Left_Rotate_D(arr,d):
-    j=0
-    n=len(arr)
-    d=d%n
-    temp=[]
-    for i in range(n):
-        temp[i]=arr[i]
+def Left_Rotate_D(arr, d):
+    n = len(arr)
+    d = d % n  # handle cases when d > n
     
-    for i in range(d,n):
-        arr[i-d]=arr[i]
+    temp = []
+    for i in range(d):  # store first d elements
+        temp.append(arr[i])
+    
+    # Shift remaining elements to the left
+    for i in range(d, n):
+        arr[i - d] = arr[i]
+    
+    # Put back temp elements at the end
+    for i in range(d):
+        arr[n - d + i] = temp[i]
+    
+    return arr
 
-
-    for i in range(n-d):
-        arr[i]=temp[i-(n-d)]
-
-
-arr=[1,2,3,4,5,6,7]
-d=3
-print(Left_Rotate_D(arr,d))
+# Example
+arr = [1, 2, 3, 4, 5, 6, 7]
+d = 3
+print(Left_Rotate_D(arr, d))
